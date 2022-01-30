9ac7e13dcc93fdd042e6fc07f16674b7ab7ae591
BharaDwaj <bdisforall@gmail.com>
2022-01-30 19:55:25 +0530

    Modified palindrome with input arguments

--- palindrome.java
+++ palindrome.java
@@ -1,20 +1,28 @@
 class palindrome{
 	public static void main(String args[]){
+
+		int x=Integer.parseInt(args[0]);
+		System.out.println(Palindrome(x));
+
+	}
+
+	public static boolean Palindrome(int x){
 		boolean ans;
-		int x=args[0];
-		if(x<0){
-			ans=false;
-		}
-		else{
-			int y=x,rev=0,rem;
-			while(y!=0){
-				rem=y%10;
-				rev=rev*10+rem;
-				y=y/10;
-			}
-			if(rev==x) ans=true;
-			else ans=false;
-		}
-		return ans;
+
+                if(x<0){
+                        ans=false;
+                }
+                else{
+                        int y=x,rev=0,rem;
+                        while(y!=0){
+                                rem=y%10;
+                                rev=rev*10+rem;
+                                y=y/10;
+                        }
+                        if(rev==x) ans=true;
+                        else ans=false;
+                }
+                return ans;
+
 	}
 }
